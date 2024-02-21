342da93ea62574d0e92375e1d50c36d1c24b68f8
mohamedii <moe@Mohamed>
2024-02-21 03:40:29 +0200

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -28,19 +28,19 @@ void insertion_sort_list(listint_t **list)
 */
 void swap(listint_t **h, listint_t **n1, listint_t *n2)
 {
-    (*n1)->next = n2->next;
-    if (n2->next != NULL)
-    {
-        n2->next->prev = *n1;
-    }
-    n2->next = *n1;
-    n2->prev = (*n1)->prev;
-    if((*n1)->prev != NULL)
-    {
-        (*n1)->prev->next = n2;
-    }
-    else
-        *h = n2;
-    (*n1)->prev = n2;
-    *n1 = n2->prev;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+	{
+	n2->next->prev = *n1;
+	}
+	n2->next = *n1;
+	n2->prev = (*n1)->prev;
+	if((*n1)->prev != NULL)
+	{
+		(*n1)->prev->next = n2;
+	}
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
