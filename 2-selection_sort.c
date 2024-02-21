9281a4ebabd9d461c7ea6dafa5d538052f5595fb
mohamedii <moe@Mohamed>
2024-02-21 04:07:42 +0200

    fixcing

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,6 +7,8 @@ void selection_sort(int *array, size_t size)
     int tmp;
     size_t i, j;
 
+    if(array == NULL)
+        return NULL;
     for(i=0; i < size; i++)
     {
         for(j = 0; j < size; j++)
