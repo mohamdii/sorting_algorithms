b1d2a06b3e71fcdaae5dd90850b6192ed303bfa5
mohamedii <moe@Mohamed>
2024-02-21 04:05:21 +0200

    s

---
+++ 2-selection_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+/**
+ * selection_sort - implements selection sort alg
+*/
+void selection_sort(int *array, size_t size)
+{
+    int tmp;
+    size_t i, j;
+
+    for(i=0; i < size; i++)
+    {
+        for(j = 0; j < size; j++)
+        {
+            if(*(array + i) >*(array +j))
+            {
+                tmp = *(array + i);
+                *(array + i) = *(array + j);
+                *(array + j) = tmp;
+            }
+        }
+    }
+}
--- test.c
+++ test.c
@@ -2,13 +2,7 @@
 
 int main(void)
 {
-    int **x, z;
-    int *y;
-    x = &y;
-    *x = y;
-    z = 10;
-    y = &z;
-    printf("%p\n%p\n%p\n%p\n%p\n", x, *x, y, &y, &z);
+ 
 
     return 1;
 }
