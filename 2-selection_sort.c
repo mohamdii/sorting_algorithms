dacc4127a7fbd9eb965f3bb257825df52a650f67
mohamedii <moe@Mohamed>
2024-02-21 04:18:15 +0200

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,22 +6,22 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    int tmp;
-    size_t i, j;
+	int tmp;
+	size_t i, j;
 
-    if(array == NULL)
-        return NULL;
-    for(i=0; i < size; i++)
-    {
-        for(j = 0; j < size; j++)
-        {
-            if(*(array + i) >*(array +j))
-            {
-                tmp = *(array + i);
-                *(array + i) = *(array + j);
-                *(array + j) = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	if(array == NULL)
+		return NULL;
+	for(i=0; i < size; i++)
+	{
+		for(j = 0; j < size; j++)
+		{
+			if(*(array + i) >*(array +j))
+	    		{
+		    		tmp = *(array + i);
+				*(array + i) = *(array + j);
+				*(array + j) = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
